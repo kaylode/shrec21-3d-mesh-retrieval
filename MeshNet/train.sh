4b4b929b46265a80c327675901a2a901f753566e
Kaylode <kaylode@github.com>
2021-02-26 18:12:02 +0700

    clean flow

--- MeshNet/train.sh
+++ MeshNet/train.sh
@@ -7,6 +7,6 @@ NUM_FACES=20000
 
 SAVED_PATH="/home/nhtlong/pmkhoi/shrec21/retrieval/MeshNet/ckpt_root/shape/20k-fold1"
 
-DATA_ROOT="$ROOT/datasets/dataset${TASK}/simplified_folds/fold_${FOLD}"
+DATA_ROOT="$ROOT/datasets/dataset${TASK}/folds/fold_${FOLD}"
 
-python train.py -r $DATA_ROOT -t $TASK --num_faces $NUM_FACES -s $SAVED_PATH -w $WEIGHT
+python train.py -r $DATA_ROOT -t $TASK --num_faces $NUM_FACES -s $SAVED_PATH
